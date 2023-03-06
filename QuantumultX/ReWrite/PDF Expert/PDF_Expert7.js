0f10cb4c21d6eb367ea9e0d262af261a85edf493
LiuBingXu1998 <lbxemail@icloud.com>
2023-03-06 13:51:34 +0800

    bug fix

--- QuantumultX/ReWrite/PDF Expert/PDF_Expert7.js
+++ QuantumultX/ReWrite/PDF Expert/PDF_Expert7.js
@@ -7,7 +7,7 @@ PDF Expert 7 Download Link：http://t.cn/A6zQCfn6
 
 [rewrite_local]
 # PDF Expert 7 解锁内购年订阅
-^https:\/\/license\.pdfexpert\.com\/api\/.*\/pdfexpert6\/subscription\/(check$|refresh$) url script-response-body PDFExpert7.js
+^https:\/\/license\.pdfexpert\.com\/api\/.*\/pdfexpert6\/subscription\/(check$|refresh$) url https://github.com/LiuBingXu1998/Agent-Software-configuration/blob/main/QuantumultX/ReWrite/PDF%20Expert/PDF_Expert7.js
 
 [mitm]
 hostname = license.pdfexpert.com,
@@ -17,14 +17,14 @@ hostname = license.pdfexpert.com,
 let obj = JSON.parse($response.body);
 
 obj = {
-  "isEligibleForIntroPeriod":true,
-  "originalTransactionId":"730000355000000",
-  "subscriptionExpirationDate":"01:01 01/01/2020",
-  "subscriptionState":"active",
-  "subscriptionReceiptId":"1559207500000",
-  "isPDFExpert6User":true,
-  "subscriptionAutoRenewStatus":"autoRenewOff",
-  "isInGracePeriod":true
+  isEligibleForIntroPeriod: true,
+  originalTransactionId: '730000355000000',
+  subscriptionExpirationDate: '01:01 01/01/2020',
+  subscriptionState: 'active',
+  subscriptionReceiptId: '1559207500000',
+  isPDFExpert6User: true,
+  subscriptionAutoRenewStatus: 'autoRenewOff',
+  isInGracePeriod: true,
 };
 
-$done({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
